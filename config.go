ede4f16ea185b76b4462ab8b588c43241bf5f501
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-05-08 06:00:07 +0300

    feat: add config validation

--- config.go
+++ config.go
@@ -1,7 +1,9 @@
 package dcpg
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
 type Config struct {
@@ -27,6 +29,63 @@ type SlotConfig struct {
 	Create bool   `json:"create" yaml:"create"`
 }
 
-func (c Config) DSN() string {
+func (c *Config) DSN() string {
 	return fmt.Sprintf("postgres://%s:%s@%s/%s?replication=database", c.Username, c.Password, c.Host, c.Database)
+}
+
+func (c *Config) SetDefault() {
+	if c.ChannelBuffer == 0 {
+		c.ChannelBuffer = 1000
+	}
+}
+
+func (c *Config) Validate() error {
+	var err error
+	if isEmpty(c.Host) {
+		err = errors.Join(err, errors.New("host cannot be empty"))
+	}
+
+	if isEmpty(c.Username) {
+		err = errors.Join(err, errors.New("username cannot be empty"))
+	}
+
+	if isEmpty(c.Password) {
+		err = errors.Join(err, errors.New("password cannot be empty"))
+	}
+
+	if isEmpty(c.Database) {
+		err = errors.Join(err, errors.New("database cannot be empty"))
+	}
+
+	if cErr := c.Publication.Validate(); cErr != nil {
+		err = errors.Join(err, cErr)
+	}
+
+	if cErr := c.Slot.Validate(); cErr != nil {
+		err = errors.Join(err, cErr)
+	}
+
+	return nil
+}
+
+func (pc *PublicationConfig) Validate() error {
+	var err error
+	if isEmpty(pc.Name) {
+		err = errors.Join(err, errors.New("publication name cannot be empty"))
+	}
+
+	return err
+}
+
+func (sc *SlotConfig) Validate() error {
+	var err error
+	if isEmpty(sc.Name) {
+		err = errors.Join(err, errors.New("slot name cannot be empty"))
+	}
+
+	return err
+}
+
+func isEmpty(s string) bool {
+	return strings.TrimSpace(s) == ""
 }

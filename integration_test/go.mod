4f950478d2b56898cbdc263e61e25ed86c0e0d82
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-06-09 16:37:27 +0300

    feat: add transactional process integration test

--- integration_test/go.mod
+++ integration_test/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/Trendyol/go-pq-cdc v0.0.0-00010101000000-000000000000
 	github.com/go-playground/errors v3.3.0+incompatible
 	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/jackc/pgx/v5 v5.6.0
 	github.com/stretchr/testify v1.9.0
 	github.com/testcontainers/testcontainers-go v0.31.0
 )
@@ -41,7 +42,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
-	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
@@ -72,6 +73,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
---
+++ integration_test/transactional_process_test.go
@@ -0,0 +1,103 @@
+package integration
+
+import (
+	"context"
+	cdc "github.com/Trendyol/go-pq-cdc"
+	"github.com/Trendyol/go-pq-cdc/config"
+	"github.com/Trendyol/go-pq-cdc/pq"
+	"github.com/Trendyol/go-pq-cdc/pq/message/format"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestTransactionalProcess(t *testing.T) {
+	ctx := context.Background()
+
+	messageCh := make(chan any, 500)
+	handlerFunc := func(ctx pq.ListenerContext) {
+		switch msg := ctx.Message.(type) {
+		case *format.Insert, *format.Delete, *format.Update:
+			messageCh <- msg
+		}
+		_ = ctx.Ack()
+	}
+
+	connector, err := cdc.NewConnector(ctx, Config, handlerFunc)
+	assert.NoError(t, err)
+
+	cfg := config.Config{Host: Config.Host, Username: "postgres", Password: "postgres", Database: Config.Database}
+	pool, err := pgxpool.New(ctx, cfg.DSNWithoutSSL())
+	assert.NoError(t, err)
+
+	t.Cleanup(func() {
+		connector.Close()
+		err = RestoreDB(ctx)
+		assert.NoError(t, err)
+
+		pool.Close()
+	})
+
+	go func() {
+		connector.Start(ctx)
+	}()
+
+	waitCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	err = connector.WaitUntilReady(waitCtx)
+	assert.NoError(t, err)
+	cancel()
+
+	t.Run("Start transactional operation and commit. Then check the messages and metrics", func(t *testing.T) {
+		tx, err := pool.Begin(ctx)
+		assert.NoError(t, err)
+
+		_, err = tx.Exec(ctx, "INSERT INTO books (id, name) VALUES (12, 'j*va is best')")
+		assert.NoError(t, err)
+
+		_, err = tx.Exec(ctx, "UPDATE books SET name = 'go is best' WHERE id = 12")
+		assert.NoError(t, err)
+
+		err = tx.Commit(ctx)
+		assert.NoError(t, err)
+
+		insertMessage := <-messageCh
+		assert.Equal(t, map[string]any{"id": int32(12), "name": "j*va is best"}, insertMessage.(*format.Insert).Decoded)
+		updateMessage := <-messageCh
+		assert.Equal(t, map[string]any{"id": int32(12), "name": "go is best"}, updateMessage.(*format.Update).NewDecoded)
+
+		updateMetric, _ := fetchUpdateOpMetric()
+		insertMetric, _ := fetchInsertOpMetric()
+		deleteMetric, _ := fetchDeleteOpMetric()
+		assert.True(t, updateMetric == 1)
+		assert.True(t, insertMetric == 1)
+		assert.True(t, deleteMetric == 0)
+	})
+
+	t.Run("Start transactional operation and rollback. Then Delete book which id is 12. Then check the messages and metrics", func(t *testing.T) {
+		tx, err := pool.Begin(ctx)
+		assert.NoError(t, err)
+
+		_, err = tx.Exec(ctx, "INSERT INTO books (id, name) VALUES (13, 'j*va is best')")
+		assert.NoError(t, err)
+
+		_, err = tx.Exec(ctx, "UPDATE books SET name = 'go is best' WHERE id = 13")
+		assert.NoError(t, err)
+
+		err = tx.Rollback(ctx)
+		assert.NoError(t, err)
+
+		_, err = pool.Exec(ctx, "DELETE FROM books WHERE id = 12")
+		assert.NoError(t, err)
+
+		deleteMessage := <-messageCh
+		assert.Equal(t, int32(12), deleteMessage.(*format.Delete).OldDecoded["id"])
+
+		updateMetric, _ := fetchUpdateOpMetric()
+		insertMetric, _ := fetchInsertOpMetric()
+		deleteMetric, _ := fetchDeleteOpMetric()
+		assert.True(t, updateMetric == 1)
+		assert.True(t, insertMetric == 1)
+		assert.True(t, deleteMetric == 1)
+	})
+}

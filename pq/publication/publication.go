525265e7bfbff783da0d8553ac5be3cc49c7325d
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-06-18 21:19:38 +0300

    feat: add publication info decoder

--- pq/publication/publication.go
+++ pq/publication/publication.go
@@ -5,12 +5,16 @@ import (
 	goerrors "errors"
 	"github.com/Trendyol/go-pq-cdc/pq"
 	"github.com/go-playground/errors"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgtype"
 	"log/slog"
 )
 
 var (
-	ErrorPublicationIsNotExists = errors.New("publication is not exists")
+	ErrorPublicationIsNotExists = goerrors.New("publication is not exists")
 )
+
+var typeMap = pgtype.NewMap()
 
 type Publication struct {
 	cfg  Config
@@ -51,6 +55,9 @@ func (c *Publication) Info(ctx context.Context) (*Config, error) {
 	resultReader := c.conn.Exec(ctx, c.cfg.infoQuery())
 	results, err := resultReader.ReadAll()
 	if err != nil {
+		if v, ok := err.(*pgconn.PgError); ok && v.Code == "42703" {
+			return nil, ErrorPublicationIsNotExists
+		}
 		return nil, errors.Wrap(err, "publication info result")
 	}
 
@@ -62,5 +69,50 @@ func (c *Publication) Info(ctx context.Context) (*Config, error) {
 		return nil, errors.Wrap(err, "publication info result reader close")
 	}
 
-	return nil, nil
+	publicationInfo, err := decodePublicationInfoResult(results[0])
+	if err != nil {
+		return nil, errors.Wrap(err, "publication info result decode")
+	}
+
+	return publicationInfo, nil
+}
+
+func decodePublicationInfoResult(result *pgconn.Result) (*Config, error) {
+	var publicationConfig Config
+	for i, fd := range result.FieldDescriptions {
+		v, err := decodeTextColumnData(result.Rows[0][i], fd.DataTypeOID)
+		if err != nil {
+			return nil, err
+		}
+
+		if v == nil {
+			continue
+		}
+
+		switch fd.Name {
+		case "pubname":
+			publicationConfig.Name = v.(string)
+		case "pubinsert":
+			publicationConfig.Insert = v.(bool)
+		case "pubupdate":
+			publicationConfig.Update = v.(bool)
+		case "pubdelete":
+			publicationConfig.Delete, _ = v.(bool)
+		case "pubtruncate":
+			publicationConfig.Truncate, _ = v.(bool)
+		case "included_tables":
+			for _, val := range v.([]any) {
+				publicationConfig.IncludedTables = append(publicationConfig.IncludedTables, val.(string))
+			}
+		}
+	}
+
+	return &publicationConfig, nil
+}
+
+func decodeTextColumnData(data []byte, dataType uint32) (interface{}, error) {
+	if dt, ok := typeMap.TypeForOID(dataType); ok {
+		return dt.Codec.DecodeValue(typeMap, dataType, pgtype.TextFormatCode, data)
+	}
+	return string(data), nil
 }

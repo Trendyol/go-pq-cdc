22f6485ca96b61777a256c7940d42a6ed56313e2
briankski <brian.kalinowski@flocksafety.com>
2026-03-19 11:32:41 -0700

    fix example dir

---
+++ example/simple/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	cdc "github.com/Trendyol/go-pq-cdc"
+	"github.com/Trendyol/go-pq-cdc/config"
+	"github.com/Trendyol/go-pq-cdc/pq/message/format"
+	"github.com/Trendyol/go-pq-cdc/pq/publication"
+	"github.com/Trendyol/go-pq-cdc/pq/replication"
+	"github.com/Trendyol/go-pq-cdc/pq/slot"
+)
+
+/*
+	psql "postgres://cdc_user:cdc_pass@127.0.0.1/cdc_db?replication=database"
+
+	CREATE TABLE users (
+	 id serial PRIMARY KEY,
+	 name text NOT NULL,
+	 created_on timestamptz
+	);
+
+	INSERT INTO users (name)
+	SELECT
+		'Oyleli' || i
+	FROM generate_series(1, 100) AS i;
+*/
+
+func main() {
+	ctx := context.Background()
+	cfg := config.Config{
+		Host:      "127.0.0.1",
+		Port:      5433,
+		Username:  "cdc_user",
+		Password:  "cdc_pass",
+		Database:  "cdc_db",
+		DebugMode: false,
+		Publication: publication.Config{
+			CreateIfNotExists: true,
+			Name:              "cdc_publication",
+			Operations: publication.Operations{
+				publication.OperationInsert,
+				publication.OperationDelete,
+				publication.OperationTruncate,
+				publication.OperationUpdate,
+			},
+			Tables: publication.Tables{
+				publication.Table{
+					Name:            "users",
+					ReplicaIdentity: publication.ReplicaIdentityDefault,
+					Schema:          "public",
+				},
+			},
+		},
+		Slot: slot.Config{
+			CreateIfNotExists:           true,
+			Name:                        "cdc_slot",
+			SlotActivityCheckerInterval: 3000,
+		},
+		Metric: config.MetricConfig{
+			Port: 8081,
+		},
+		Logger: config.LoggerConfig{
+			LogLevel: slog.LevelInfo,
+		},
+	}
+
+	connector, err := cdc.NewConnector(ctx, cfg, Handler)
+	if err != nil {
+		slog.Error("new connector", "error", err)
+		os.Exit(1)
+	}
+
+	defer connector.Close()
+	connector.Start(ctx)
+}
+
+func Handler(ctx *replication.ListenerContext) {
+	switch msg := ctx.Message.(type) {
+	case *format.Insert:
+		slog.Info("insert message received", "new", msg.Decoded)
+	case *format.Delete:
+		slog.Info("delete message received", "old", msg.OldDecoded)
+	case *format.Update:
+		slog.Info("update message received", "new", msg.NewDecoded, "old", msg.OldDecoded)
+	}
+
+	if err := ctx.Ack(); err != nil {
+		slog.Error("ack", "error", err)
+	}
+}
